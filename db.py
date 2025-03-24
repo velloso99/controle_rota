fe8400f46b4a99bb01c00534b46f757242e1bded
Rodrigo Lopes Velloso <velloso.rl9@outlook.com>
2025-03-24 17:55:56 -0300

    main

---
+++ db.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+# Criar conexão
+try: 
+    con = sqlite3.connect('calculo_rota.db')
+    print("Conexão com Banco de Dados efetuado com sucesso!")
+except sqlite3.Error as e:
+    print("Erro ao se conectar com Banco de Dados!")
+
+# Criar tabelas do Banco de Dados
+# Tabela de Login--------------------------------------
+try:
+    with con:
+        cur = con.cursor()
+        
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS lucro_entregas(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                data TEXT,
+                valor_rota TEXT,
+                km_rodado TEXT,
+                calculo TEXT,
+                valor_combustivel TEXT,
+                total_combustivel TEXT,
+                total TEXT
+            )""")
+        print("Tabela de lucro_entregas criado com sucesso!")
+except sqlite3.Error as e:
+    print("Erro ao criar tabela de lucro_entregas!")
+
+
+
+
+
+
+
+
---
+++ imports.py
@@ -0,0 +1,10 @@
+from tkinter import*
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import Tk
+from tkinter import ttk
+from tkinter import Scrollbar
+
+from views import*
+from sqlite3 import*
